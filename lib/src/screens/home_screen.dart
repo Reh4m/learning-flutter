42b59ad7fc32f06605c64ac8566506f82db26c50
emmarp <ruiizemmanuel@gmail.com>
2025-03-18 15:41:05 -0600

    Feat (HomeScreen): Add 'logout' button and clear shared preferences on pressed

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  Future<void> logout() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: const Center(child: Text('Home Screen')));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Screen'),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.brightness_4_outlined)),
+          IconButton(
+            onPressed: () {
+              logout();
+
+              Navigator.pushReplacementNamed(context, '/sign-in');
+            },
+            icon: Icon(Icons.logout),
+          ),
+        ],
+      ),
+    );
   }
 }
