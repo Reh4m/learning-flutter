015cf0ebecb81140284ec7eff6c1b5b1ad8b0747
emmarp <ruiizemmanuel@gmail.com>
2025-03-18 16:24:04 -0600

    Style: Light theme colors setup

---
+++ lib/src/themes/light_theme.dart
@@ -0,0 +1,20 @@
+// Flutter custom light theme based on blue color
+import 'package:flutter/material.dart';
+
+class LightTheme {
+  static const Color backgroundColor = Color(0xFFE0E0E0);
+  static const Color cardBackgroundColor = Color(0xFFFFFFFF);
+
+  static const Color primaryColor = Color(0xFF007AAD);
+  static const Color primaryColorLight = Color(0xFFD9E2E9);
+  static const Color onPrimaryColor = Color(0xFFFFFFFF);
+
+  static const Color textPrimary = Color(0xFF313131);
+  static const Color textPrimaryLight = Color(0xFFA2A2A2);
+  static const Color textSecondary = Color(0xFF242424);
+
+  static const Color success = Color(0xFF34C759);
+  static const Color warning = Color(0xFFFF9500);
+  static const Color error = Color(0xFFFF3B30);
+  static const Color info = Color(0xFF5AC8FA);
+}
