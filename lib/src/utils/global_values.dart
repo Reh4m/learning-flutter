fcad57634161e95c7814745853abb12435c3966f
emmarp <ruiizemmanuel@gmail.com>
2025-03-18 23:15:28 -0600

    Chore (GlobalValues): Set Light theme as default 'themeApp' value

--- lib/src/utils/global_values.dart
+++ lib/src/utils/global_values.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:learning_flutter/src/themes/theme.dart';
 
 class GlobalValues {
-  static ValueNotifier themeApp = ValueNotifier(ThemeData.light());
+  static ValueNotifier themeApp = ValueNotifier(AppTheme.lightTheme);
 }
