be375faf592376e418b1c2f6108f7ea0585aa844
emmarp <ruiizemmanuel@gmail.com>
2025-03-19 01:28:23 -0600

    GlobalValues: Add 'colorMode' property

--- lib/src/utils/global_values.dart
+++ lib/src/utils/global_values.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:learning_flutter/src/themes/theme.dart';
 
+enum ColorMode { light, dark, system }
+
 class GlobalValues {
   static ValueNotifier themeApp = ValueNotifier(AppTheme.lightTheme);
+  static ValueNotifier colorMode = ValueNotifier(ColorMode.light);
 }
