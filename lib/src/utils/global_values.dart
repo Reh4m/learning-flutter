86d30bf4d7d6b8baa737f77c878b566bd4023b98
emmarp <ruiizemmanuel@gmail.com>
2025-03-19 16:19:42 -0600

    GlobalValues: 'primaryColorTheme' added

--- lib/src/utils/global_values.dart
+++ lib/src/utils/global_values.dart
@@ -4,6 +4,8 @@ import 'package:learning_flutter/src/themes/theme.dart';
 enum ColorMode { light, dark, system }
 
 class GlobalValues {
+  // Theme and color mode
   static ValueNotifier themeApp = ValueNotifier(AppTheme.lightTheme);
   static ValueNotifier colorMode = ValueNotifier(ColorMode.light);
+  static ValueNotifier primaryColorTheme = ValueNotifier(Color(0xFF007AAD));
 }
