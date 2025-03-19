ee7924176bede47263c4eef39870772f89244d2f
emmarp <ruiizemmanuel@gmail.com>
2025-03-19 17:28:26 -0600

    Custom theme colors added

---
+++ lib/src/themes/custom_themes.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class DefaultTheme {
+  static const Color primaryColor = Color(0xFF007AAD);
+  static const Color primaryColorLight = Color(0xFFD9E2E9);
+}
+
+class SunsetOrangeTheme {
+  static const Color primaryColor = Color(0xFFFF5733);
+  static const Color primaryColorLight = Color(0xFFFFE1D8);
+}
+
+class EmeraldGreenTheme {
+  static const Color primaryColor = Color(0xFF2ECC71);
+  static const Color primaryColorLight = Color(0xFFDDF6E3);
+}
+
+class RoyalPurpleTheme {
+  static const Color primaryColor = Color(0xFF6C5CE7);
+  static const Color primaryColorLight = Color(0xFFDEE0FD);
+}
+
+class GoldenYellowTheme {
+  static const Color primaryColor = Color(0xFFFFC300);
+  static const Color primaryColorLight = Color(0xFFFFF4DB);
+}
+
+class MidnightTheme {
+  static const Color primaryColor = Color(0xFF212529);
+  static const Color primaryColorLight = Color(0xFFCDCED0);
+}
