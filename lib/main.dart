39ecfd9c800fe1c08e2f41f34d84abe6680c1c3d
emmarp <ruiizemmanuel@gmail.com>
2025-03-18 23:20:42 -0600

    Feat (Main): Load App Theme value from SharedPreferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:learning_flutter/src/config/routes.dart';
+import 'package:learning_flutter/src/themes/theme.dart';
+import 'package:learning_flutter/src/utils/global_values.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+
+  String theme = prefs.getString('theme') ?? 'light';
+
+  GlobalValues.themeApp.value =
+      theme == 'dark' ? AppTheme.darkTheme : AppTheme.lightTheme;
+
   runApp(const MyApp());
 }
 
@@ -10,11 +22,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Learning Flutter',
-      onGenerateRoute: Routes.generateRoute,
-      initialRoute: '/splash-screen',
+    return ValueListenableBuilder(
+      valueListenable: GlobalValues.themeApp,
+      builder:
+          (context, value, child) => MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Learning Flutter',
+            theme: value,
+            onGenerateRoute: Routes.generateRoute,
+            initialRoute: '/splash-screen',
+          ),
     );
   }
 }
