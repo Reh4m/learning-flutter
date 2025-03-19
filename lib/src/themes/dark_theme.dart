06a813ba817aa4f87970433a711288bd44e3c6d6
emmarp <ruiizemmanuel@gmail.com>
2025-03-18 22:11:55 -0600

    Style: Dark theme colors setup

---
+++ lib/src/themes/dark_theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class DarkTheme {
+  static const Color backgroundColor = Color(0xFF121212);
+  static const Color cardBackgroundColor = Color(0xFF1E1E1E);
+
+  static const Color primaryColor = Color(0xFF007AAD);
+  static const Color primaryColorLight = Color(0xFF005F85);
+  static const Color onPrimaryColor = Color(0xFFFFFFFF);
+
+  static const Color textPrimary = Color(0xFFE0E0E0);
+  static const Color textPrimaryLight = Color(0xFFB0B0B0);
+  static const Color textSecondary = Color(0xFFCCCCCC);
+
+  static const Color greyLighten1 = Color(0xFF2C2C2C);
+  static const Color greyLighten2 = Color(0xFF3A3A3A);
+  static const Color greyLighten3 = Color(0xFF505050);
+
+  static const Color success = Color(0xFF34C759);
+  static const Color warning = Color(0xFFFFA726);
+  static const Color error = Color(0xFFEF5350);
+  static const Color info = Color(0xFF5AC8FA);
+}
