7f1b6e189ebe4e2d683448c660f3dc3a6cf5d66b
emmarp <ruiizemmanuel@gmail.com>
2025-03-20 15:04:17 -0600

    Refactor: Remove global values

--- lib/src/utils/global_values.dart
+++ lib/src/utils/global_values.dart
@@ -1,11 +1,1 @@
-import 'package:flutter/material.dart';
-import 'package:learning_flutter/src/themes/theme.dart';
-import 'package:learning_flutter/src/themes/theme_colors.dart';
-import 'package:learning_flutter/src/utils/theme_provider.dart';
-
-class GlobalValues {
-  // Theme and color mode
-  static ValueNotifier themeApp = ValueNotifier(AppTheme.lightTheme);
-  static ValueNotifier colorMode = ValueNotifier(ColorMode.light);
-  static ValueNotifier themeColor = ValueNotifier(ThemeColor.defaultTheme);
-}
+class GlobalValues {}
