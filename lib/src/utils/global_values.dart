457b2813bacf01dfd8c55ebf5287b92d11131a79
emmarp <ruiizemmanuel@gmail.com>
2025-03-19 21:51:24 -0600

    Refactor (GlobalValues): Use 'ThemeManager' for custom theme color

--- lib/src/utils/global_values.dart
+++ lib/src/utils/global_values.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:learning_flutter/src/themes/theme.dart';
+import 'package:learning_flutter/src/themes/theme_manager.dart';
 
 enum ColorMode { light, dark, system }
 
@@ -7,5 +8,5 @@ class GlobalValues {
   // Theme and color mode
   static ValueNotifier themeApp = ValueNotifier(AppTheme.lightTheme);
   static ValueNotifier colorMode = ValueNotifier(ColorMode.light);
-  static ValueNotifier primaryColorTheme = ValueNotifier(Color(0xFF007AAD));
+  static ValueNotifier themeColor = ValueNotifier(ThemeColor.defaultTheme);
 }
