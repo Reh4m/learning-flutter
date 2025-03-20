021cd65bbd1bf2b0ef3dfff4253dc54897602f5a
emmarp <ruiizemmanuel@gmail.com>
2025-03-20 03:47:32 -0600

    Refactor (Main): Load custom app theme using Theme Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:learning_flutter/src/config/routes.dart';
-import 'package:learning_flutter/src/themes/theme_manager.dart';
-import 'package:learning_flutter/src/utils/global_values.dart';
+import 'package:learning_flutter/src/utils/theme_provider.dart';
+import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  SharedPreferences prefs = await SharedPreferences.getInstance();
+  final prefs = await SharedPreferences.getInstance();
 
-  String themeMode = prefs.getString('theme') ?? 'light';
-  String themeColor = prefs.getString('themeColor') ?? 'defaultTheme';
+  final themeProvider = ThemeProvider(prefs);
+  await themeProvider.loadAppTheme();
 
-  ColorMode themeModeEnum =
-      themeMode == 'dark' ? ColorMode.dark : ColorMode.light;
-  ThemeColor themeColorEnum =
-      themeColor == 'defaultTheme'
-          ? ThemeColor.defaultTheme
-          : ThemeColor.values.byName(themeColor);
-
-  CustomTheme customTheme = ThemeManager.getTheme(themeColorEnum);
-
-  GlobalValues.themeApp.value = ThemeManager.getThemeInstance(
-    customTheme,
-    themeModeEnum,
+  runApp(
+    ChangeNotifierProvider(create: (_) => themeProvider, child: const MyApp()),
   );
-  GlobalValues.colorMode.value = themeModeEnum;
-  GlobalValues.themeColor.value = themeColorEnum;
-
-  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -36,16 +22,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ValueListenableBuilder(
-      valueListenable: GlobalValues.themeApp,
-      builder:
-          (context, value, child) => MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: 'Learning Flutter',
-            theme: value,
-            onGenerateRoute: Routes.generateRoute,
-            initialRoute: '/splash-screen',
-          ),
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Learning Flutter',
+      theme: themeProvider.currentAppTheme,
+      onGenerateRoute: Routes.generateRoute,
+      initialRoute: '/splash-screen',
     );
   }
 }
