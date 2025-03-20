e06c1169c2cd93fbc79afa04a4c68c6b072ae25a
emmarp <ruiizemmanuel@gmail.com>
2025-03-19 22:48:37 -0600

    Chore (Main): Load 'themeColor' from Shared Preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:learning_flutter/src/config/routes.dart';
 import 'package:learning_flutter/src/themes/theme.dart';
+import 'package:learning_flutter/src/themes/theme_manager.dart';
 import 'package:learning_flutter/src/utils/global_values.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -9,12 +10,39 @@ void main() async {
 
   SharedPreferences prefs = await SharedPreferences.getInstance();
 
-  String theme = prefs.getString('theme') ?? 'light';
+  String themeMode = prefs.getString('theme') ?? 'light';
+  String themeColor = prefs.getString('themeColor') ?? 'defaultTheme';
+
+  ThemeColor themeColorEnum =
+      themeColor == 'defaultTheme'
+          ? ThemeColor.defaultTheme
+          : ThemeColor.values.byName(themeColor);
+
+  CustomTheme customTheme = ThemeManager.getTheme(themeColorEnum);
+
+  ThemeData darkThemeInstance = AppTheme.darkTheme.copyWith(
+    primaryColor: customTheme.primaryColor,
+    primaryColorLight: customTheme.primaryColorLight,
+    colorScheme: AppTheme.darkTheme.colorScheme.copyWith(
+      primary: customTheme.primaryColor,
+      secondary: customTheme.primaryColorLight,
+    ),
+  );
+
+  ThemeData lightThemeInstance = AppTheme.lightTheme.copyWith(
+    primaryColor: customTheme.primaryColor,
+    primaryColorLight: customTheme.primaryColorLight,
+    colorScheme: AppTheme.lightTheme.colorScheme.copyWith(
+      primary: customTheme.primaryColor,
+      secondary: customTheme.primaryColorLight,
+    ),
+  );
 
   GlobalValues.themeApp.value =
-      theme == 'dark' ? AppTheme.darkTheme : AppTheme.lightTheme;
+      themeMode == 'dark' ? darkThemeInstance : lightThemeInstance;
   GlobalValues.colorMode.value =
-      theme == 'dark' ? ColorMode.dark : ColorMode.light;
+      themeMode == 'dark' ? ColorMode.dark : ColorMode.light;
+  GlobalValues.themeColor.value = themeColorEnum;
 
   runApp(const MyApp());
 }
