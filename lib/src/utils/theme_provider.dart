1674bdaa37a9a18c0c549a9303b9f95c99abeb54
emmarp <ruiizemmanuel@gmail.com>
2025-03-20 02:35:54 -0600

    Refactor: Removed Theme Manager, using Theme Provider instead

---
+++ lib/src/utils/theme_provider.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:learning_flutter/src/themes/theme.dart';
+import 'package:learning_flutter/src/themes/theme_colors.dart';
+import 'package:learning_flutter/src/themes/theme_fonts.dart';
+import 'package:learning_flutter/src/utils/global_values.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  final SharedPreferences prefs;
+  ThemeData _currentAppTheme = AppTheme.lightTheme;
+
+  ThemeData get currentAppTheme => _currentAppTheme;
+
+  // Current theme values
+  ColorMode _colorMode = ColorMode.light;
+  ThemeColor _themeColor = ThemeColor.defaultTheme;
+  String _fontFamily = 'Roboto';
+
+  ColorMode get colorMode => _colorMode;
+  ThemeColor get primaryColorLight => _themeColor;
+  String get fontFamily => _fontFamily;
+
+  ThemeProvider(this.prefs);
+
+  Future<void> loadAppTheme() async {
+    // Load saved values from SharedPreferences
+    String colorMode = prefs.getString('theme') ?? 'light';
+    String themeColor = prefs.getString('themeColor') ?? 'defaultTheme';
+    String fontFamily = prefs.getString('fontFamily') ?? 'Roboto';
+
+    // Set the current theme values
+    _colorMode = colorMode == 'light' ? ColorMode.light : ColorMode.dark;
+    _themeColor = ThemeColor.values.byName(themeColor);
+    _fontFamily = fontFamily;
+
+    _updateAppTheme();
+  }
+
+  void _updateAppTheme() {
+    final TextTheme textTheme = _getTextTheme();
+
+    if (_colorMode == ColorMode.light) {
+      _currentAppTheme = AppTheme.lightTheme.copyWith(
+        primaryColor: ThemeColors.getCustomTheme(_themeColor).primaryColor,
+        primaryColorLight:
+            ThemeColors.getCustomTheme(_themeColor).primaryColorLight,
+        textTheme: textTheme,
+      );
+    }
+
+    if (_colorMode == ColorMode.dark) {
+      _currentAppTheme = AppTheme.darkTheme.copyWith(
+        primaryColor: ThemeColors.getCustomTheme(_themeColor).primaryColor,
+        primaryColorLight:
+            ThemeColors.getCustomTheme(_themeColor).primaryColorLight,
+        textTheme: textTheme,
+      );
+    }
+
+    notifyListeners();
+  }
+
+  TextTheme _getTextTheme() {
+    return ThemeFonts.getTextTheme(_fontFamily);
+  }
+
+  Future<void> updateColorMode(ColorMode colorMode) async {
+    _colorMode = colorMode;
+
+    await prefs.setString(
+      'theme',
+      colorMode == ColorMode.light ? 'light' : 'dark',
+    );
+
+    _updateAppTheme();
+  }
+
+  Future<void> updateThemeColor(ThemeColor themeColor) async {
+    _themeColor = themeColor;
+
+    await prefs.setString('themeColor', themeColor.name);
+
+    _updateAppTheme();
+  }
+
+  Future<void> updateFontFamily(String fontFamily) async {
+    _fontFamily = fontFamily;
+
+    await prefs.setString('fontFamily', fontFamily);
+
+    _updateAppTheme();
+  }
+}
