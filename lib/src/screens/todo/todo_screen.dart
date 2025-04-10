37cbfd62143e2237fc103467cb735993c759e63f
Reh4m <ruiizemmanuel@gmail.com>
2025-04-10 13:56:47 -0600

    Refactor (TodoList): Extract widgets into separate method widgets

--- lib/src/screens/todo/todo_screen.dart
+++ lib/src/screens/todo/todo_screen.dart
@@ -13,7 +13,7 @@ class TodoScreen extends StatefulWidget {
 }
 
 class _TodoScreenState extends State<TodoScreen> {
-  late TodoProvider? todoProvider;
+  late TodoProvider todoProvider;
 
   @override
   void initState() {
@@ -22,28 +22,31 @@ class _TodoScreenState extends State<TodoScreen> {
     todoProvider = TodoProvider();
   }
 
-  void _toggleTaskStatus(TodoModel todoTask) {
-    todoProvider!.toggleStatus(todoTask.id, !todoTask.status).then((value) {
-      if (value > 0) {
-        GlobalValues.updateTodoList.value = !GlobalValues.updateTodoList.value;
+  void _toggleTaskStatus(TodoModel todoTask) async {
+    final result = await todoProvider.toggleStatus(
+      todoTask.id,
+      !todoTask.status,
+    );
 
-        _showSnackBar(todoTask.status ? 'Task uncompleted' : 'Task completed');
-      }
-    });
+    if (result > 0) {
+      GlobalValues.updateTodoList.value = !GlobalValues.updateTodoList.value;
+
+      _showSnackBar(todoTask.status ? 'Task uncompleted' : 'Task completed');
+    }
   }
 
   void _editTask(TodoModel todoTask) {
     Navigator.pushNamed(context, '/todo-form', arguments: todoTask);
   }
 
-  void _deleteTask(TodoModel todoTask) {
-    todoProvider!.delete(todoTask.id).then((value) {
-      if (value > 0) {
-        GlobalValues.updateTodoList.value = !GlobalValues.updateTodoList.value;
+  void _deleteTask(TodoModel todoTask) async {
+    final result = await todoProvider.delete(todoTask.id);
 
-        _showSnackBar('Task deleted');
-      }
-    });
+    if (result > 0) {
+      GlobalValues.updateTodoList.value = !GlobalValues.updateTodoList.value;
+
+      _showSnackBar('Task deleted');
+    }
   }
 
   void _showSnackBar(String message) {
@@ -56,18 +59,13 @@ class _TodoScreenState extends State<TodoScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Todo List')),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => Navigator.pushNamed(context, '/todo-form'),
-        shape: CircleBorder(),
-        backgroundColor: Theme.of(context).primaryColor,
-        child: const Icon(Icons.add),
-      ),
+      floatingActionButton: _buildFloatingActionButton(),
       body: SafeArea(
         child: ValueListenableBuilder(
           valueListenable: GlobalValues.updateTodoList,
           builder: (context, value, child) {
             return FutureBuilder<List<TodoModel>>(
-              future: todoProvider!.fetch(),
+              future: todoProvider.fetch(),
               builder: (context, snapshot) {
                 if (snapshot.hasError) {
                   return Center(child: Text(snapshot.error.toString()));
@@ -77,92 +75,103 @@ class _TodoScreenState extends State<TodoScreen> {
                   return const Center(child: CircularProgressIndicator());
                 }
 
-                return ListView.builder(
-                  padding: EdgeInsets.all(10),
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: (context, index) {
-                    TodoModel todoTask = snapshot.data![index];
-
-                    return Slidable(
-                      key: Key(todoTask.id.toString()),
-                      closeOnScroll: true,
-                      startActionPane: ActionPane(
-                        motion: const ScrollMotion(),
-                        children: <Widget>[
-                          SlidableAction(
-                            onPressed: (context) => _toggleTaskStatus(todoTask),
-                            backgroundColor:
-                                todoTask.status
-                                    ? LightTheme.warning
-                                    : LightTheme.success,
-                            foregroundColor:
-                                Theme.of(context).colorScheme.onPrimary,
-                            icon:
-                                todoTask.status
-                                    ? Icons.cancel
-                                    : Icons.check_circle,
-                            label: todoTask.status ? 'Uncomplete' : 'Complete',
-                          ),
-                        ],
-                      ),
-                      endActionPane: ActionPane(
-                        motion: const ScrollMotion(),
-                        children: <Widget>[
-                          SlidableAction(
-                            onPressed: (context) => _editTask(todoTask),
-                            backgroundColor: LightTheme.info,
-                            foregroundColor:
-                                Theme.of(context).colorScheme.onPrimary,
-                            icon: Icons.edit,
-                            label: 'Edit',
-                          ),
-                          SlidableAction(
-                            onPressed: (context) => _deleteTask(todoTask),
-                            backgroundColor: LightTheme.warning,
-                            foregroundColor:
-                                Theme.of(context).colorScheme.onPrimary,
-                            icon: Icons.delete,
-                            label: 'Delete',
-                          ),
-                        ],
-                      ),
-                      child: ListTile(
-                        isThreeLine: true,
-                        leading: Icon(
-                          todoTask.status ? Icons.check_circle : Icons.cancel,
-                          color:
-                              todoTask.status
-                                  ? LightTheme.success
-                                  : LightTheme.warning,
-                        ),
-                        title: Text(
-                          todoTask.title,
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w600,
-                            decoration:
-                                todoTask.status
-                                    ? TextDecoration.lineThrough
-                                    : TextDecoration.none,
-                          ),
-                        ),
-                        subtitle: Text(
-                          todoTask.description,
-                          style: const TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                        trailing: Text(todoTask.date),
-                      ),
-                    );
-                  },
-                );
+                return _buildTodoList(snapshot.data!);
               },
             );
           },
         ),
       ),
+    );
+  }
+
+  Widget _buildFloatingActionButton() {
+    return FloatingActionButton(
+      onPressed: () => Navigator.pushNamed(context, '/todo-form'),
+      shape: const CircleBorder(),
+      backgroundColor: Theme.of(context).primaryColor,
+      child: const Icon(Icons.add),
+    );
+  }
+
+  Widget _buildTodoList(List<TodoModel> todoList) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(10),
+      itemCount: todoList.length,
+      itemBuilder: (context, index) {
+        final todoTask = todoList[index];
+
+        return _buildTodoItem(todoTask);
+      },
+    );
+  }
+
+  Widget _buildTodoItem(TodoModel todoTask) {
+    return Slidable(
+      key: Key(todoTask.id.toString()),
+      closeOnScroll: true,
+      startActionPane: _buildStartActionPane(todoTask),
+      endActionPane: _buildEndActionPane(todoTask),
+      child: ListTile(
+        isThreeLine: true,
+        leading: Icon(
+          todoTask.status ? Icons.check_circle : Icons.cancel,
+          color: todoTask.status ? LightTheme.success : LightTheme.warning,
+        ),
+        title: Text(
+          todoTask.title,
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+            decoration:
+                todoTask.status
+                    ? TextDecoration.lineThrough
+                    : TextDecoration.none,
+          ),
+        ),
+        subtitle: Text(
+          todoTask.description,
+          style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
+        ),
+        trailing: Text(todoTask.date),
+      ),
+    );
+  }
+
+  ActionPane _buildStartActionPane(TodoModel todoTask) {
+    return ActionPane(
+      motion: const ScrollMotion(),
+      children: [
+        SlidableAction(
+          onPressed: (context) => _toggleTaskStatus(todoTask),
+          backgroundColor:
+              todoTask.status ? LightTheme.warning : LightTheme.success,
+          foregroundColor: Theme.of(context).colorScheme.onPrimary,
+          icon: todoTask.status ? Icons.cancel : Icons.check_circle,
+          label: todoTask.status ? 'Uncomplete' : 'Complete',
+        ),
+      ],
+    );
+  }
+
+  ActionPane _buildEndActionPane(TodoModel todoTask) {
+    return ActionPane(
+      motion: const ScrollMotion(),
+      children: [
+        SlidableAction(
+          onPressed: (context) => _editTask(todoTask),
+          backgroundColor: LightTheme.info,
+          foregroundColor: Theme.of(context).colorScheme.onPrimary,
+          icon: Icons.edit,
+          label: 'Edit',
+        ),
+        SlidableAction(
+          onPressed: (context) => _deleteTask(todoTask),
+          backgroundColor: LightTheme.warning,
+          foregroundColor: Theme.of(context).colorScheme.onPrimary,
+          icon: Icons.delete,
+          label: 'Delete',
+        ),
+      ],
     );
   }
 }
