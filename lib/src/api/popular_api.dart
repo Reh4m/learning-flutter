a038492e54cf26a7140a2f2de90ca9e663a0acc1
Reh4m <ruiizemmanuel@gmail.com>
2025-04-10 16:37:27 -0600

    Chore (PopularApi): Add method to fetch movie genres

--- lib/src/api/popular_api.dart
+++ lib/src/api/popular_api.dart
@@ -1,9 +1,24 @@
 import 'package:dio/dio.dart';
 import 'package:learning_flutter/secret/keys.dart' as secret;
+import 'package:learning_flutter/src/models/movie_genre_model.dart';
 import 'package:learning_flutter/src/models/popular_model.dart';
 
 class PopularApi {
   final dio = Dio();
+
+  Future<List<MovieGenreModel>> getHttpGenres() async {
+    try {
+      Response response = await dio.get(
+        'https://api.themoviedb.org/3/genre/movie/list?api_key=${secret.themoviedbApi}&language=en-US',
+      );
+
+      List genres = response.data['genres'];
+
+      return genres.map((genre) => MovieGenreModel.fromMap(genre)).toList();
+    } catch (e) {
+      return [];
+    }
+  }
 
   Future<List<PopularModel>> getHttpPopular() async {
     try {
