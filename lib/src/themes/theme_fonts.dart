d3a71b73290186a2518adecc0aa404a810ca5a29
Reh4m <ruiizemmanuel@gmail.com>
2025-07-22 14:44:18 -0600

    Feat (ThemeFont): Create 'FontFamily' enum

--- lib/src/themes/theme_fonts.dart
+++ lib/src/themes/theme_fonts.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+enum FontFamily { roboto, lato, openSans, montserrat, poppins }
+
 class ThemeFonts {
-  static final Map<String, TextTheme> _textThemes = {
-    'Roboto': GoogleFonts.robotoTextTheme(),
-    'Lato': GoogleFonts.latoTextTheme(),
-    'Open Sans': GoogleFonts.openSansTextTheme(),
-    'Montserrat': GoogleFonts.montserratTextTheme(),
-    'Poppins': GoogleFonts.poppinsTextTheme(),
+  static final Map<FontFamily, TextTheme> _textThemes = {
+    FontFamily.roboto: GoogleFonts.robotoTextTheme(),
+    FontFamily.lato: GoogleFonts.latoTextTheme(),
+    FontFamily.openSans: GoogleFonts.openSansTextTheme(),
+    FontFamily.montserrat: GoogleFonts.montserratTextTheme(),
+    FontFamily.poppins: GoogleFonts.poppinsTextTheme(),
   };
 
   static TextTheme getTextTheme(String fontFamily, Brightness themeMode) {
