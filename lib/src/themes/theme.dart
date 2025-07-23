067942d3f35b7fdac4704a2bdcda0cd9d8293835
Reh4m <ruiizemmanuel@gmail.com>
2025-07-22 19:00:53 -0600

    Refactor (AppTheme): Replace 'generate' methods with getters

--- lib/src/themes/theme.dart
+++ lib/src/themes/theme.dart
@@ -1,74 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:learning_flutter/src/themes/dark_theme.dart';
 import 'package:learning_flutter/src/themes/light_theme.dart';
-import 'package:learning_flutter/src/themes/theme_fonts.dart';
 
 class AppTheme {
-  const AppTheme();
+  static ThemeData get lightTheme => ThemeData(
+    useMaterial3: true,
+    brightness: Brightness.light,
+    primaryColor: LightTheme.primaryColor,
+    primaryColorLight: LightTheme.primaryColorLight,
+    scaffoldBackgroundColor: LightTheme.backgroundColor,
+    colorScheme: const ColorScheme.light(
+      primary: LightTheme.primaryColor,
+      secondary: LightTheme.primaryColorLight,
+      surface: LightTheme.greyLighten2,
+      error: LightTheme.error,
+      onPrimary: LightTheme.onPrimaryColor,
+      onSecondary: LightTheme.onPrimaryColor,
+      onSurface: LightTheme.textPrimary,
+      onError: LightTheme.onPrimaryColor,
+    ),
+    textTheme: GoogleFonts.robotoTextTheme(),
+    cardColor: LightTheme.cardBackgroundColor,
+    // Text Form Input
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor: LightTheme.greyLighten2,
+      labelStyle: const TextStyle(color: LightTheme.textSecondary),
+      border: UnderlineInputBorder(
+        borderSide: BorderSide.none,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    ),
+  );
 
-  static ThemeData generateLightTheme(FontFamily fontFamily) {
-    return ThemeData(
-      useMaterial3: true,
-      brightness: Brightness.light,
-      primaryColor: LightTheme.primaryColor,
-      primaryColorLight: DarkTheme.primaryColorLight,
-      scaffoldBackgroundColor: LightTheme.backgroundColor,
-      colorScheme: ColorScheme(
-        brightness: Brightness.light,
-        primary: LightTheme.primaryColor,
-        secondary: LightTheme.primaryColorLight,
-        surface: LightTheme.greyLighten2,
-        error: LightTheme.error,
-        onPrimary: LightTheme.onPrimaryColor,
-        onSecondary: LightTheme.onPrimaryColor,
-        onSurface: LightTheme.textPrimary,
-        onError: LightTheme.onPrimaryColor,
+  static ThemeData get darkTheme => ThemeData(
+    useMaterial3: true,
+    brightness: Brightness.dark,
+    primaryColor: DarkTheme.primaryColor,
+    primaryColorLight: DarkTheme.primaryColorLight,
+    scaffoldBackgroundColor: DarkTheme.backgroundColor,
+    colorScheme: const ColorScheme.dark(
+      primary: DarkTheme.primaryColor,
+      secondary: DarkTheme.primaryColorLight,
+      surface: DarkTheme.cardBackgroundColor,
+      error: DarkTheme.error,
+      onPrimary: DarkTheme.onPrimaryColor,
+      onSecondary: DarkTheme.onPrimaryColor,
+      onSurface: DarkTheme.textPrimary,
+      onError: DarkTheme.onPrimaryColor,
+    ),
+    textTheme: GoogleFonts.robotoTextTheme(),
+    cardColor: DarkTheme.cardBackgroundColor,
+    // Text Form Input
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor: DarkTheme.cardBackgroundColor,
+      labelStyle: const TextStyle(color: DarkTheme.textSecondary),
+      border: UnderlineInputBorder(
+        borderSide: BorderSide.none,
+        borderRadius: BorderRadius.circular(10.0),
       ),
-      textTheme: ThemeFonts.getTextTheme(fontFamily, Brightness.light),
-      cardColor: LightTheme.cardBackgroundColor,
-      // Text Form Input
-      inputDecorationTheme: InputDecorationTheme(
-        filled: true,
-        fillColor: LightTheme.greyLighten2,
-        labelStyle: TextStyle(color: LightTheme.textSecondary),
-        border: UnderlineInputBorder(
-          borderSide: BorderSide.none,
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-      ),
-    );
-  }
-
-  static ThemeData generateDarkTheme(FontFamily fontFamily) {
-    return ThemeData(
-      useMaterial3: true,
-      brightness: Brightness.dark,
-      primaryColor: DarkTheme.primaryColor,
-      primaryColorLight: DarkTheme.primaryColorLight,
-      scaffoldBackgroundColor: DarkTheme.backgroundColor,
-      colorScheme: ColorScheme(
-        brightness: Brightness.dark,
-        primary: DarkTheme.primaryColor,
-        secondary: DarkTheme.primaryColorLight,
-        surface: DarkTheme.cardBackgroundColor,
-        error: DarkTheme.error,
-        onPrimary: DarkTheme.onPrimaryColor,
-        onSecondary: DarkTheme.onPrimaryColor,
-        onSurface: DarkTheme.textPrimary,
-        onError: DarkTheme.onPrimaryColor,
-      ),
-      textTheme: ThemeFonts.getTextTheme(fontFamily, Brightness.dark),
-      cardColor: DarkTheme.cardBackgroundColor,
-      // Text Form Input
-      inputDecorationTheme: InputDecorationTheme(
-        filled: true,
-        fillColor: DarkTheme.cardBackgroundColor,
-        labelStyle: TextStyle(color: DarkTheme.textSecondary),
-        border: UnderlineInputBorder(
-          borderSide: BorderSide.none,
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-      ),
-    );
-  }
+    ),
+  );
 }
